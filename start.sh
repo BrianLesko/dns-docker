bb300346fad69c76d59e8327c716e9c8184a7bb8
Brian Lesko <brianjosephlesko@gmail.com>
2024-04-26 20:33:02 -0400

    pain

--- start.sh
+++ start.sh
@@ -3,6 +3,11 @@
 # Build Docker image
 docker build -t dns .
 
-# Run Docker container
-docker run --name dns -d -p 53:53/udp -p 53:53/tcp -v "$(pwd)/zones:/etc/bind/zones" bind9-dns
+# Docker run 
+docker run -d --name dns \
+  -v $(pwd)/config:/etc/bind \
+  -v $(pwd)/cache:/var/cache/bind \
+  -v $(pwd)/records:/var/lib/bind \
+  -p 53:53/tcp -p 53:53/udp \
+  ubuntu/bind9:latest
 
